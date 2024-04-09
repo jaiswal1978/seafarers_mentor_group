6bbe242ce6104eacc804d63cbfbeb5e7f2152c1d
Amit Jaiswal <a.jaiswal78@gmail.com>
2024-04-09 08:51:29 +0530

    updated data from the Google form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import Data from "./Data";
+
+
+const AccordionItem = (props) => {
+    const contentEl = useRef();
+    const { handleToggle, active, faq } = props;
+    const { header, id, contactNumber, whatsAppNumber, email, prefferedMode } = faq;
+    const iconStyle = { color: '#007cc4' };
+
+    return (
+        <div className="rc-accordion-card">
+            <div className="rc-accordion-header">
+                <div className={`rc-accordion-toggle p-3 ${active === id ? 'active' : ''}`} onClick={() => handleToggle(id)}>
+                    <h5 className="rc-accordion-title">{header}</h5>
+                    <i className="fa fa-chevron-down rc-accordion-icon"></i>
+                </div>
+            </div>
+            <div ref={contentEl} className={`rc-collapse ${active === id ? 'show' : ''}`} style={
+                active === id
+                    ? { height: contentEl.current.scrollHeight }
+                    : { height: "0px" }
+            }>
+                <div className="rc-accordion-body">
+                  <div className="Social-media pb-3">
+                    <a href={`tel:${contactNumber}`}><FontAwesomeIcon icon={faPhone} style={iconStyle} /></a>
+                    <a href={`https://wa.me/${whatsAppNumber}`}><FontAwesomeIcon icon={faWhatsapp} style={iconStyle} /></a>
+                    <a href={`mailto:${email}`}><FontAwesomeIcon icon={faEnvelope} style={iconStyle} /></a>
+                    </div>
+                    <div className="container">
+                        <div className="contact-info">
+                            <p>Contact Number:</p>
+                            <p>{contactNumber}</p>
+                        </div>
+                    </div>
+                    <hr/>
+                    <div className="container">
+                        <div className="contact-info">
+                            <p>WhatsApp Number:</p>
+                            <p>{whatsAppNumber}</p>
+                        </div>
+                    </div>
+                    <hr/>
+                    <div className="container">
+                        <div className="contact-info">
+                            <p>Email Id:</p>
+                            <p>{email}</p>
+                        </div>
+                    </div>
+                    <hr/>
+                    <div className="container">
+                        <div className="contact-info">
+                            <p>Preffered mode of Contact:</p>
+                            <p>{prefferedMode}</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [active, setActive] = useState(null);
+
+    const handleToggle = (index) => {
+        if (active === index) {
+            setActive(null);
+        } else {
+            setActive(index);
+        }
+    }
+
+    return (
+        <>
+            <div className="container-fluid mt-5 mb-5">
+                <div className="row justify-content-center">
+                    <div className="col-md-8 mt-2">
+                        <div className="card">
+                            <div className="card-body">
+                              <h4 className="form-heading text-primary text-center mt-3">Seafarers India Mentoring Group</h4>
+                              <h5 className="form-heading text-primary text-center">Contact Details</h5>
+                                {Data.map((faq, index) => (
+                                    <AccordionItem key={index} active={active} handleToggle={handleToggle} faq={faq} />
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App;
---
+++ src/Data.js
@@ -0,0 +1,99 @@
+const Data = [
+    {
+        id: 1,
+        header: "Capt. Iype Kovoor",
+        contactNumber: "+919819086858",
+        whatsAppNumber: "919819086858",
+        email: "iikovoor@gmail.com",
+        prefferedMode: ""
+      },
+      {
+        id: 2,
+        header: "Captain Amresh Jha",
+        contactNumber: "+919136097311",
+        whatsAppNumber: "919136097311",
+        email: "seafarers.india2021@gmail.com",
+        prefferedMode: "WhatsApp, Email, Phone Call"
+      },
+      {
+        id: 3,
+        header: "Prateek Saxena",
+        contactNumber: "+919871203758",
+        whatsAppNumber: "918653769787",
+        email: "marinerprateeksaxena26@gmail.com",
+        prefferedMode: "WhatsApp, Email, Phone Call, Google Meet / Zoom"
+      },
+      {
+        id: 4,
+        header: "Ashav Lohany",
+        contactNumber: "+919657441697",
+        whatsAppNumber: "919657441697",
+        email: "ashav.lohany99@gmail.com",
+        prefferedMode: "WhatsApp, Email, Phone Call, Google Meet / Zoom"
+      },
+      {
+        id: 5,
+        header: "Capt Gajanan Karanjikar",
+        contactNumber: "+917506413035",
+        whatsAppNumber: "07506413035",
+        email: "captgajanan@gmail.com",
+        prefferedMode: "WhatsApp, Email, Google Meet / Zoom"
+      },
+      {
+        id: 6,
+        header: "Rajkumar Sahoo",
+        contactNumber: "+919566519573",
+        whatsAppNumber: "919566519573",
+        email: "raj687401@gmail.com",
+        prefferedMode: "WhatsApp, Email, Phone Call, Google Meet / Zoom"
+      },
+      {
+        id: 7,
+        header: "Sachin Saurav",
+        contactNumber: "+918882698258",
+        whatsAppNumber: "918882698258",
+        email: "sachinsauravsss@gmail.com",
+        prefferedMode: "WhatsApp, Email, Google Meet / Zoom"
+      },
+      {
+        id: 8,
+        header: "Thomas Gipson paiva",
+        contactNumber: "+918281530727",
+        whatsAppNumber: "918281530727",
+        email: "gipsonpaiva77@gmail.com",
+        prefferedMode: "WhatsApp, Email"
+      },
+      {
+        id: 9,
+        header: "Saurav Kumar",
+        contactNumber: "+918092867449",
+        whatsAppNumber: "918092867449",
+        email: "sauravf40@yahoo.co.in",
+        prefferedMode: "WhatsApp"
+      },
+      {
+        id: 10,
+        header: "Rohit Ranjan",
+        contactNumber: "+918709020610",
+        whatsAppNumber: "918709020610",
+        email: "rohitranjan42000@gmail.com",
+        prefferedMode: "WhatsApp, Email, Phone Call, Google Meet / Zoom"
+      },
+      {
+        id: 11,
+        header: "Vivek Narain",
+        contactNumber: "+919971230110",
+        whatsAppNumber: "919971230110",
+        email: "viveknarain@navirego.com",
+        prefferedMode: "WhatsApp, Email, Phone Call, Google Meet / Zoom"
+      },
+      {
+        id: 12,
+        header: "Amit Jaiswal",
+        contactNumber: "+919410947070",
+        whatsAppNumber: "919410947070",
+        email: "a.jaiswal78@gmail.com",
+        prefferedMode: "WhatsApp, Email, Google Meet / Zoom"
+      },
+]
+export default Data;
